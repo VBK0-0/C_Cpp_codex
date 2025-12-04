791ef66de4ec85c28ed111ff53c2a60272ae0b88
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-04 14:26:19 +0530

    commit

--- Week_1/Code/modify_operators.c
+++ Week_1/Code/modify_operators.c
@@ -7,22 +7,22 @@ main()
 
     x = 10, y = 10;
     y = x++;
-    printf("%d, %d\n", x, y);
+    printf("%d, %d\n", x, y); //11, 10
 
     x =10, y =20; 
     z = x++ * --y;
-    printf("%d,%d,%d\n",x, y, z);
+    printf("%d,%d,%d\n",x, y, z); //11,19,190
     
     x = 5;
     x = ++x + x++;
-    printf("%d\n",x);
+    printf("%d\n",x); //13
 
     a = 2, b = 3;
     b = a++ + b--;
-    printf("%d, %d\n",a, b);
+    printf("%d, %d\n",a, b); //3, 5
     a = a-- + ++b;
-    printf("%d, %d\n",a, b);
+    printf("%d, %d\n",a, b); //9, 6
     b = ++a + --b;
-    printf("%d, %d\n",a, b);
+    printf("%d, %d\n",a, b); //10, 15
 
 }
