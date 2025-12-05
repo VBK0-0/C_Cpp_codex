11d7781e05836f8c700b623c39d86fae87c7fbc3
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-05 15:30:47 +0530

    Commit

---
+++ Week_1/Code/Printing_Strong_Numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    long int limit, n, i, temp, r, fact, sum;
+    printf("Enter the limit: ");
+    scanf("%d", &limit);
+    for(n=1; n<=limit; n++)
+    {
+        temp = n;
+        sum = 0;
+        while(temp>0)
+        {
+            r = temp%10;
+            fact = 1;
+            for(i = r; i>=1; i--)
+            {
+                fact = fact*i;
+            }
+            sum = sum + fact;
+            temp = temp/10;
+        }
+        if(n==sum)
+        printf("%d, ",n);
+    } 
+}
