2ac311571f2df6c0f4d0a30c364710582620bd62
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-05 19:29:53 +0530

    commit

---
+++ Week_1/Code/Factorial_using_recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+    int n, res;
+    printf("Enter n value: ");
+    scanf("%d", &n);
+    res = fact(n);
+    printf("Result is %d", res);
+}
+int fact(int n)
+{
+    int res;
+    if(n ==0)
+        res =1;
+    else
+        res = n*fact(n-1);
+    return res;
+}
