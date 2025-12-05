5c218909660225c07ec998d1f0c30f08f1dd7397
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-05 12:23:30 +0530

    commit

---
+++ Week_1/Code/Sum_of_digits_in_a_given_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n, r, sum =0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    while(n<0)
+    {
+        printf("Invalid!\nPlease enter a positive integer:");
+        scanf("%d",&n);
+    }
+
+        int temp =n;
+        while(temp>0)
+        {
+            r = temp %10;
+            sum = sum + r;
+            temp = temp/10;
+        }
+        printf("The sum of the digits of %d is: %d ", n, sum);
+    
+}
