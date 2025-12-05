2ca29f088c3079e62642f6511d998d21019d5fe4
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-05 11:20:05 +0530

    commit

---
+++ Week_1/Code/Printing_Patterns_5.c
@@ -0,0 +1,30 @@
+/*Pattern:
+Ex: Enter the value of n:8
+                                       *
+                                      ***
+                                     *****
+                                    *******
+                                   *********
+                                  ***********
+                                 *************
+                                ***************
+*/
+#include<stdio.h>
+int main()
+{
+    int n, i, j, k, c =80;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=((c/2)-i);j++)
+        {
+            printf(" ");
+        }
+        for(k=1; k<=(2*i-1);k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
