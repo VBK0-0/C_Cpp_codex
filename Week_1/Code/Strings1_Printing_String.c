aa0a6d16c27817bdcbfc1b0a7964567453ead577
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-12-07 11:15:25 +0530

    commit

---
+++ Week_1/Code/Strings1_Printing_String.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+    char name[20];
+printf("Enter your name: ");
+gets(name); // Takes input until enter is pressed, and stores into an array.
+printf("Hi %s!! Welcome", name);
+}
